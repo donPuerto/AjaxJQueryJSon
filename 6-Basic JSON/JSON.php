60de87a4d6d889213c900632e99d8ac244924c5b
Don Puerto <don.puerto@hotmail.com>
2015-12-23 20:32:40 +0800

    Demo Complete

---
+++ 6-Basic JSON/JSON.php
@@ -0,0 +1,22 @@
+<?php
+header('Content-Type:application/json');
+?>
+
+{
+	"contactDetails" : {
+		"firstname": "John",
+		"lastname": "Doe",
+		"jobtitle": "IT Engineer",
+		"company": "Creative Panda",
+		"emailAdd": "john.doe@gmail.com",
+		"mobileNumber": "09215398226",
+		"billingAddress" : {
+			"streetAddress": "New York St",
+			"suburb":"Gold Coast",
+			"postalCode": "4031",
+			"Country": "New York City"
+
+		}
+	}
+	
+}
